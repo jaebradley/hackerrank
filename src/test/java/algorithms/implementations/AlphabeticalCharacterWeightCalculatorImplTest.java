ccc6e1148643e6d42b1d2b611a043b7747c8bb91
jaebradley <jae.b.bradley@gmail.com>
2017-05-07 19:28:10 -0400

    add test

---
+++ src/test/java/algorithms/implementations/AlphabeticalCharacterWeightCalculatorImplTest.java
@@ -0,0 +1,24 @@
+package algorithms.implementations;
+
+import algorithms.interfaces.AlphabeticalCharacterWeightCalculator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlphabeticalCharacterWeightCalculatorImplTest {
+  AlphabeticalCharacterWeightCalculator calculator = new AlphabeticalCharacterWeightCalculatorImpl();
+
+  @Test
+  public void itShouldThrowForNonAlphabeticCharacter() {
+    try {
+      calculator.calculate('1');
+    } catch (RuntimeException e) {
+      // expected
+    }
+  }
+
+  @Test
+  public void itShouldCalculateWeight() {
+    assertEquals(calculator.calculate('A'), 1);
+  }
+}
